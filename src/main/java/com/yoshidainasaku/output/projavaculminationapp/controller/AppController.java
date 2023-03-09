ab0254f7dbdeb8204f8f694ebe941a78250c9234
吉田稲作 <yoshidanoinasaku@gmail.com>
2023-03-09 16:09:17 +0900

    "/"のエンドポイントを作成し、それがうまく動作できるかテストコードを作成した

---
+++ src/main/java/com/yoshidainasaku/output/projavaculminationapp/controller/AppController.java
@@ -0,0 +1,12 @@
+package com.yoshidainasaku.output.projavaculminationapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping("/")
+    String home() {
+        return "home";
+    }
+}
