be35f575f3da1df49e677413aa7f6d187dfc372f
吉田稲作 <yoshidanoinasaku@gmail.com>
2023-03-14 18:15:12 +0900

    ユーザーテーブルを作成し、アプリケーション起動時にユーザーを一人作成する

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+INSERT INTO users (user_id, user_name, email, password)
+    VALUES ('1', 'tester', 'example.com', '39fcbad0-495b-4f8c-a776-912045524e87');
+
 INSERT INTO tasklist (id, task, deadline, done)
     VALUES ('1', 'MySQLの導入', '2023-03-13', false),
            ('2', 'ログインページの作成', '2023-03-17', false),
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,20 @@
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS tasklist;
 
+--ユーザー
+CREATE TABLE IF NOT EXISTS users (
+    user_id VARCHAR(8) PRIMARY KEY,
+    user_name VARCHAR(128) NOT NULL UNIQUE,
+    email VARCHAR(256) NOT NULL,
+    password VARCHAR(128) NOT NULL
+);
+
+-- タスクリスト
 CREATE TABLE IF NOT EXISTS tasklist (
     id VARCHAR(8) PRIMARY KEY,
     task VARCHAR(256),
     deadline VARCHAR(10),
     done BOOLEAN
 );
+
+--ToDo: ユーザーテーブルとタスクリストの紐付け
