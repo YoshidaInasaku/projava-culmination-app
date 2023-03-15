2182f40ba3507384e374e908f21d64f1382787bc
吉田稲作 <yoshidanoinasaku@gmail.com>
2023-03-15 14:43:51 +0900

    アプリ起動時に初期化するデータ（パスワード）を変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (user_id, user_name, email, password)
-    VALUES ('1', 'tester', 'example.com', '39fcbad0-495b-4f8c-a776-912045524e87');
+    VALUES ('1', 'tester', 'example.com', '$2a$10$brwjno3JJ4042zwnadLUcu9RooTuj83jWqhu/cqZuOPBOBTPl/L12');
 
 INSERT INTO tasklist (id, task, deadline, done)
     VALUES ('1', 'MySQLの導入', '2023-03-13', false),
